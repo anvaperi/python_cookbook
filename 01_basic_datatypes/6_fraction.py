dba164a3c8b9b336812f3ca04fb214bbc3a6bc54
anvaperi <donantonio5314@gmail.com>
2023-04-09 00:35:53 +0200

    completed basic data types

--- 01_basic_datatypes/6_fraction.py
+++ 01_basic_datatypes/6_fraction.py
@@ -15,4 +15,21 @@ f = Fraction(9,5)
 print(f.numerator, f.denominator)
 print(Fraction('3.1415926535897932').limit_denominator(1000))
 
+def reduced_fraction(fraction):
+	return (fraction.numerator // fraction.denominator) + \
+		(Fraction(fraction.numerator % fraction.denominator, fraction.denominator))*1J
+
+def print_reduced_fraction(fraction):
+	print(' + '.join([str(i) for i in (reduced_fraction(fraction))]))
+
+print(reduced_fraction(f))
+print(reduced_fraction(Fraction(2,3)))
+print(reduced_fraction(Fraction(20,3)))
+
+def combine_fraction(in_tuple):
+  return Fraction(in_tuple[0] * in_tuple[1].denominator + in_tuple[1].numerator, in_tuple[1].denominator)
+
+my_tuple = (6, Fraction(2,3))
+print(combine_fraction(my_tuple))
+
 print('int attrs:\n',  '\n'.join(dir(Fraction)))
